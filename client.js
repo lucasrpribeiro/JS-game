0f27736488c2608b0590cce1b7f1e0bd10629b63
Lucas <lucas.ribeiro711@gmail.com>
2022-08-22 22:56:27 -0300

    feat: implement observer pattern

--- client.js
+++ client.js
@@ -1,33 +1,113 @@
-const gameScreen = document.getElementById("screen"); // Selecionando o canva do html
-const context = gameScreen.getContext("2d"); // Criando o contexto de render
+const gameScreen = document.getElementById("screen");
+const context = gameScreen.getContext("2d");
+const currentPlayerId = "player1";
 
-const game = {
-  players: {
-    player1: { x: 1, y: 1 },
-    player2: { x: 9, y: 9 },
-  },
-  fruits: {
-    fruit1: { x: 3, y: 1 },
-  },
-}; // Definindo o objeto de jogo que vai conter as variaveis do meu jogo
+// ==============
+// CREATE A GAME
+// ==============
 
+function createGame() {
+  const state = {
+    players: {
+      player1: { x: 1, y: 1 },
+      player2: { x: 9, y: 9 },
+    },
+    fruits: {
+      fruit1: { x: 3, y: 1 },
+    },
+  };
+  function movePlayer(command) {
+    console.log(`moving ${command.playerId} to ${command.keyPressed}`);
+
+    const keyPressed = command.keyPressed;
+    const player = state.players[command.playerId];
+
+    if (keyPressed === "ArrowUp" && player.y - 1 >= 0) {
+      player.y = player.y - 1;
+      return;
+    }
+    if (keyPressed === "ArrowRight" && player.x + 1 < gameScreen.width) {
+      player.x = player.x + 1;
+      return;
+    }
+    if (keyPressed === "ArrowDown" && player.y + 1 < gameScreen.height) {
+      player.y = player.y + 1;
+      return;
+    }
+    if (keyPressed === "ArrowLeft" && player.x - 1 >= 0) {
+      player.x = player.x - 1;
+      return;
+    }
+  }
+
+  return {
+    movePlayer,
+    state,
+  };
+}
+
+const game = createGame();
+const keyboardListenner = createKeyboardListenner();
+keyboardListenner.subscribe(game.movePlayer);
+
+// ==============
+// INPUT SECTION
+// ==============
+
+function createKeyboardListenner() {
+  const state = {
+    observers: [],
+  };
+
+  function subscribe(observerFunction) {
+    state.observers.push(observerFunction);
+  }
+
+  function notifyAll(command) {
+    console.log(`Notify ${state.observers.length} observers`);
+
+    for (const observerFunction of state.observers) {
+      observerFunction(command);
+    }
+  }
+
+  document.addEventListener("keydown", handleKeyDown);
+
+  function handleKeyDown(event) {
+    const keyPressed = event.key;
+
+    const command = {
+      playerId: "player1", // HARDCODED
+      keyPressed,
+    };
+
+    notifyAll(command);
+  }
+
+  return {
+    subscribe,
+  };
+}
+// ==================
+// RENDER THE SCREEN
+// ==================
 renderScreen();
 
 function renderScreen() {
   context.fillStyle = "white";
   context.clearRect(0, 0, 10, 10);
 
-  for (const playerId in game.players) {
-    const player = game.players[playerId];
+  for (const playerId in game.state.players) {
+    const player = game.state.players[playerId];
     context.fillStyle = "black";
     context.fillRect(player.x, player.y, 1, 1);
-  } // Seta uma cor e uma posição na tela para as minhas variáveis do objeto game
+  }
 
-  for (const fruitId in game.fruits) {
-    const fruit = game.fruits[fruitId];
+  for (const fruitId in game.state.fruits) {
+    const fruit = game.state.fruits[fruitId];
     context.fillStyle = "red";
     context.fillRect(fruit.x, fruit.y, 1, 1);
-  } // Seta uma cor e uma posição na tela para as minhas variáveis do objeto game
+  }
 
   requestAnimationFrame(renderScreen);
 }
